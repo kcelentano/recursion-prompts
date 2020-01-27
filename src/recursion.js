38ee1805a16320f3d40d66f938216b73d7a2cf3a
kcelentano <katharinec@gmail.com>
2020-01-27 13:12:58 -0800

    Complete Palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -154,6 +154,28 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	
+	string = string.toLowerCase();
+
+	if(string.length === 2){
+		if(string[0] === string[1]){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	else if(string.length === 1){
+		return true;
+	}
+	else{
+		if(string[0] === string[string.length - 1]){
+			return palindrome(string.slice(1, -1));
+		}
+		else{
+			return false;
+		}
+	}
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
